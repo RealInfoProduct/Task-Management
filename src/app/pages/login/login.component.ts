a5da8d89ebf2b7d2e5e6df5e6fd3502af8786ce5
DipakPansheriya <dkpanseriya61@gmail.com>
2023-07-21 09:40:22 +0530

    dashboard card design

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -52,71 +52,76 @@ export class LoginComponent implements OnInit {
   }
 
 
-  submit() { 
+  submit() {
     this.isLoading = true
-    if(this.employeeList.length > 0){
-        this.employeeList.forEach((element:any) => {
-  
-          if(element.emaployeeUserName === this.loginForm.value.email && element.emaployeePassword === this.loginForm.value.password && element.selectEmployeeStatus === "active" ) {
-            this.allCompanyEmployees.push(element)
-            const payload: EmaployeeList = {
-              id: element.id,
-              emaployeeName: element.emaployeeName,
-              emaployeeMobile: element.emaployeeMobile,
-              emaployeeEmail: element.emaployeeEmail,
-              emaployeeUserName: element.emaployeeUserName,
-              emaployeePassword: element.emaployeePassword,
-              selectEmployeeRole: element.selectEmployeeRole,
-              selectEmployeeTechnology: element.selectEmployeeTechnology,
-              selectEmployeeStatus: element.selectEmployeeStatus,
-              selectProject: element.selectProject,
-              selectProjectRole: element.selectProjectRole,
-              avatarName : element.avatarName,
-              isActive : 'online'
-            }    
-        
-            this.firebaseService.updateEmaployeeList(element.id, payload).then((res: any) => {
-        
-            })
-            localStorage.setItem('employeeId' ,element.id)
-            this.router.navigate(['web/employee-admin'])
-            this.CompanyLogin = true
-          }
-        })
-    }
+    if (this.employeeList.length > 0) {
+      this.employeeList.forEach((element: any) => {
 
-    if(!this.CompanyLogin){
-      this.authService.signIn(this.loginForm.value.email, this.loginForm.value.password).subscribe((res) => {
-        if(res){
-            const setItem = this.userList.find((id: any) => id.email === this.loginForm.value.email)
-            const date1 = moment(setItem.endDate);
-            const date2 = moment();
-            if(date2.isBefore(date1) && setItem.status === 'active' && setItem.userRole === 'user'){
-              localStorage.setItem('companyId', setItem.id)
-              this.router.navigate(['web/dashboard'])
-            }
-            else {
-              this.messageService.add({
-                severity: msgType.error,
-                summary:'Error' ,
-                detail: 'Account not activetd',
-                life: 1500,
-              });
-            }
-            this.isLoading = false
+        if (element.emaployeeUserName === this.loginForm.value.email && element.emaployeePassword === this.loginForm.value.password && element.selectEmployeeStatus === "active") {
+          this.allCompanyEmployees.push(element)
+          const payload: EmaployeeList = {
+            id: element.id,
+            emaployeeName: element.emaployeeName,
+            emaployeeMobile: element.emaployeeMobile,
+            emaployeeEmail: element.emaployeeEmail,
+            emaployeeUserName: element.emaployeeUserName,
+            emaployeePassword: element.emaployeePassword,
+            selectEmployeeRole: element.selectEmployeeRole,
+            selectEmployeeTechnology: element.selectEmployeeTechnology,
+            selectEmployeeStatus: element.selectEmployeeStatus,
+            selectProject: element.selectProject,
+            selectProjectRole: element.selectProjectRole,
+            avatarName: element.avatarName,
+            isActive: 'online'
           }
-        }, (error) => {
+
+          this.firebaseService.updateEmaployeeList(element.id, payload).then((res: any) => {
+
+          })
+          localStorage.setItem('employeeId', element.id)
+          this.router.navigate(['web/employee-admin'])
+          this.CompanyLogin = true
+        } else if (element.emaployeeUserName === this.loginForm.value.email && element.emaployeePassword === this.loginForm.value.password && element.selectEmployeeStatus === "inactive") {
           this.messageService.add({
             severity: msgType.error,
-            summary:'Error' ,
-            detail: error.error.error.message,
+            summary: 'Error',
+            detail: 'Account not activetd',
             life: 1500,
           });
-          this.isLoading = false
-        })
+        } else {
+          this.authService.signIn(this.loginForm.value.email, this.loginForm.value.password).subscribe((res) => {
+            if (res) {
+              const setItem = this.userList.find((id: any) => id.email === this.loginForm.value.email)
+              const date1 = moment(setItem.endDate);
+              const date2 = moment();
+              if (date2.isBefore(date1) && setItem.status === 'active' && setItem.userRole === 'user') {
+                localStorage.setItem('companyId', setItem.id)
+                this.router.navigate(['web/dashboard'])
+              }
+              else {
+                this.messageService.add({
+                  severity: msgType.error,
+                  summary: 'Error',
+                  detail: 'Account not activetd',
+                  life: 1500,
+                });
+              }
+              this.isLoading = false
+            }
+          }, (error) => {
+            this.messageService.add({
+              severity: msgType.error,
+              summary: 'Error',
+              detail: error.error.error.message,
+              life: 1500,
+            });
+            this.isLoading = false
+          })
+        }
+      })
     }
 
-      this.isLoading = false
+    this.isLoading = false
   }
 
 }
